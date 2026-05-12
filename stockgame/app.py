1265e6360826b27b47e221ffa56cb289d7273c9d
Social0w0 <135099942+Social0w0@users.noreply.github.com>
2026-05-13 04:21:39 +0900

    수정

--- stockgame/app.py
+++ stockgame/app.py
@@ -1013,7 +1013,7 @@ def shop_rename():
         if stock:
             total_asset += stock.price * h.quantity
 
-    fee = 1000000 + total_asset * 0.15  # 총 자산의 15%
+    fee = 100_0000 + total_asset * 0.15  # 총 자산의 15%
 
     if user.cash < fee:
         return jsonify({
